587271619fd9b548ef78f9d0540c19f216f05cdc
SantaCruzAndres <andres123nube@gmail.com>
2024-06-24 11:19:37 -0300

    Color por default del tema cambiado a blue.

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme{
   ThemeData getTheme(){
-    const  seedColor = Colors.green;
+    const  seedColor = Colors.blue;
 
 
     return ThemeData(
