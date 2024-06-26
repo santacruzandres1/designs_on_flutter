a33664c4d1a7d9cd7b646dd7b83c6c9187745598
SantaCruzAndres <andres123nube@gmail.com>
2024-06-26 17:54:15 -0300

    navigation_page added

--- lib/src/pages/pages.dart
+++ lib/src/pages/pages.dart
@@ -5,6 +5,7 @@ export 'package:design_application/src/pages/animations_page.dart';
 export 'package:design_application/src/pages/emergency_page.dart';
 export 'package:design_application/src/pages/glow_page.dart';
 export 'package:design_application/src/pages/graficas_circulares_page.dart';
+export 'package:design_application/src/pages/navigation_page.dart';
 export 'package:design_application/src/pages/page1_page.dart';
 export 'package:design_application/src/pages/slideshow_page.dart';
 export 'package:design_application/src/pages/sliver_page.dart';
