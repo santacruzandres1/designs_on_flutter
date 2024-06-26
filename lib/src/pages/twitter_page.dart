fa896b8c68f480349f55c8954ee29edf29ec7704
SantaCruzAndres <andres123nube@gmail.com>
2024-06-26 17:55:56 -0300

    refactored

--- lib/src/pages/twitter_page.dart
+++ lib/src/pages/twitter_page.dart
@@ -1,15 +1,56 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class TwitterPage extends StatelessWidget {
+class TwitterPage extends StatefulWidget {
   const TwitterPage({super.key});
+
+  @override
+  State<TwitterPage> createState() => _TwitterPageState();
+}
+
+class _TwitterPageState extends State<TwitterPage> {
+  bool _animation = false;
+
+void _toggleSnimation(){
+  setState(() {
+    _animation = !_animation;
+  });
+}
+
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xff1da1f2),
-      body: Center(
-        child: FaIcon(FontAwesomeIcons.twitter, color: Colors.white, size: 40,),
-      ),
+    return Scaffold(
+      backgroundColor: const Color(0xff1da1f2),
+      body: _bodyBuilder(),
+      floatingActionButton: _floatingActionButton(),
     );
+  }
+
+  Widget _bodyBuilder() {
+    return Center(
+      child: ZoomOut(
+        animate: _animation,
+          from: 30,
+          duration: const Duration(seconds: 1),
+          child: const FaIcon(
+            FontAwesomeIcons.twitter,
+            color: Colors.white,
+            size: 40,
+          )),
+    );
+  }
+
+  FloatingActionButton _floatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        setState(() {
+        _toggleSnimation();
+        });
+      },
+      backgroundColor: Colors.blueGrey,
+      child: const FaIcon(FontAwesomeIcons.play, color: Colors.white,),
+      );
   }
 }
